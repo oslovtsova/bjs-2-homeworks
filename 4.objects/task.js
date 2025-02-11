e2416c756a3b00158020ae2647854795c3277589
Olesya Dr <odryankova@yandex.ru>
2025-02-11 13:23:47 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student("Иван", "мужской", 19);
+let student2 = new Student("Мария", "женский", 20);
+let student3 = new Student("Василий", "мужской", 18);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	(this.marks === undefined) ? this.marks = [...marks]: this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks === undefined) {
+		return '0'
+	} else {
+		this.marks.forEach(item => sum += item);
+		return sum / this.marks.length
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
