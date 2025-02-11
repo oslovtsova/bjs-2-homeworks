283957f4e6da508fe065852901d0aa00746f7ae1
Olesya Dr <odryankova@yandex.ru>
2025-02-11 14:19:59 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -27,60 +27,72 @@
 	}
 }
 
+class Magazine extends PrintEditionItem {
+    constructor(type, name, releaseDate, pagesCount) {
+        super(type, name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, type, releaseDate, pagesCount) {
+        super(type, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
 class NovelBook extends Book {
-	constructor(author, name, releaseDate, pagesCount, state = 100, type = null) {
-		super(name, releaseDate, pagesCount);
-		this.type = "novel";
-		this.author = author;
-	}
+    constructor(author, type, releaseDate, pagesCount) {
+        super(author, type, releaseDate, pagesCount);
+        this.type = "novel";
+    }
 }
 
 class FantasticBook extends Book {
-	constructor(author, name, releaseDate, pagesCount, state = 100, type = null) {
-		super(name, releaseDate, pagesCount);
-		this.type = "fantastic";
-		this.author = author;
-	}
+    constructor(author, type, releaseDate, pagesCount) {
+        super(author, type, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
 }
 
 class DetectiveBook extends Book {
-	constructor(author, name, releaseDate, pagesCount, state = 100, type = null) {
-		super(name, releaseDate, pagesCount);
-		this.type = "detective";
-		this.author = author;
-	}
+    constructor(author, type, releaseDate, pagesCount) {
+        super(author, type, releaseDate, pagesCount);
+        this.type = "detective";
+    }
 }
 
-      /*Задание 2*/
+/*Задание 2*/
 
-      class Library {
-        constructor(name, books) {
-            this.name = name;
-            this.books = [];
-        }
-   
-        addBook(book) {
-            if (book.state > 30) {
-                this.books.push(book);
-            }
-        }
-        findBookBy(type, value) {
-            let findBook = this.books.find(book => book[type] === value);
-            if (typeof findBook === 'object') {
-                return findBook;
-            } else {
-                return null;
-            }
-        }
-   
-        giveBookByName(bookName) {
-            let giveBoook = this.books.find(book => book.name === bookName);
-            if (typeof giveBoook === 'object') {
-                let index = this.books.indexOf(giveBoook);
-                this.books.splice(index, 1);
-                return giveBoook;
-            } else {
-                return null;
-            }
+class Library {
+    constructor(name, books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
         }
     }
+    findBookBy(type, value) {
+        let findBook = this.books.find(book => book[type] === value);
+        if (typeof findBook === 'object') {
+            return findBook;
+        } else {
+            return null;
+        }
+    }
+
+    giveBookByName(bookName) {
+        let giveBoook = this.books.find(book => book.name === bookName);
+        if (typeof giveBoook === 'object') {
+            let index = this.books.indexOf(giveBoook);
+            this.books.splice(index, 1);
+            return giveBoook;
+        } else {
+            return null;
+        }
+    }
+}
