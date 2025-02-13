df91824b3c37fd183efe403181f884e7340ce6e6
Olesya Dr <odryankova@yandex.ru>
2025-02-13 19:32:42 +0300

    first commit variables

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,25 +1,35 @@
-﻿//Задача № 1
+﻿/*Задача № 1*/
+
+let add2 = (a, b) => a + b;
+let upgAdd2;
+
 function cachingDecoratorNew(func) {
-    const cache = [];
-    function wrapper(...args) {
-        const hash = md5(args);
-        let objectInCache = cache.find((item) => item.hash == hash)
-        if (objectInCache) {
-            console.log("Из кеша: " + objectInCache.value);
-            return "Из кеша: " + objectInCache.value;
-        }
-        let result = func(...args);
-        cache.push({hash: hash, value: result});
-        if (cache.length > 5) {
-            cache.shift();
-        }
-        console.log("Вычисляем: " + result);
-        return "Вычисляем: " + result;
+  let cache = [];
+  return function wrapper(...args) {
+    let hash = func(...args) + 2;
+    //ошибка тут, но если не указывать свойство, то он сохраняет сам объект
+    let objectInCache = cache.find((item) => item.hash === hash).value; 
+    if (objectInCache) {
+      console.log("Из кеша: " +objectInCache);
+      // но если его вернуть .value  то все хорошо,
+      // почему я не могу свойство сохранить в переменную???
+      return "Из кеша: " + objectInCache;  
     }
-    return wrapper;
+    let result = func(...args); 
+    cache.push({ hash: hash, value: result }); 
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
-//Задача № 2
+upgAdd2 = cachingDecoratorNew(add2);
+upgAdd2(1, 2);
+
+/*Задача № 2*/
+
 function debounceDecoratorNew(func, delay) {
   let timeoutId = null;
   wrapper.count = 0;
