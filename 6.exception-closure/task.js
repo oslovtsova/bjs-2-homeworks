b6b938307d08c9ac3258ecbb64d8ea6ac28ce4d9
Olesya Dr <odryankova@yandex.ru>
2025-02-13 19:25:45 +0300

    first commit variables

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,61 +1,41 @@
-﻿/* Задача 1 */
-
-function parseCount(num) {
-	let num1 = Number.parseFloat(num);
-	if (Number.isNaN(num1)) {
-		throw new Error("Невалидное значение");
-	} else {
-		return num1;
-	}
+﻿//Задача № 1
+function cachingDecoratorNew(func) {
+    const cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash == hash)
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({hash: hash, value: result});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
-function validateCount(num) {
-	try {
-		return parseCount(num);
-	} catch (error) {
-		return error;
-	} finally {
-
-	}
-}
-
-/* Задача 2 */
-
-class Triangle {
-	constructor(a, b, c) {
-		if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a)) {
-			throw new Error("Треугольник с такими сторонами не существует");
-		} else {
-			this.a = a;
-			this.b = b;
-			this.c = c;
-		}
-	}
-	get perimeter() {
-		let p = (this.a + this.b + this.c);
-		return p;
-	}
-	get area() {
-		let p = (this.a + this.b + this.c) / 2;
-		return +( Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) ).toFixed(3);
-	}
-}
-
-function getTriangle(a, b, c) {
-	try {
-		let trg = new Triangle(a, b, c);
-		return trg;
-	} catch (error) {
-		let err = {
-			get perimeter() {
-				return "Ошибка! Треугольник не существует";
-			},
-			get area() {
-				return "Ошибка! Треугольник не существует";
-			}
-		}
-		return err;
-	} finally {
-
-	}
+//Задача № 2
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  function wrapper(...args){
+    wrapper.allCount++;
+    if(timeoutId) {
+        clearTimeout(timeoutId);
+    } else {
+        func(...args);
+        wrapper.count++;
+    }
+    timeoutId = setTimeout(() => {
+        func(...args);
+        wrapper.count++;
+    }, delay);
+  }
+  return wrapper;
 }
