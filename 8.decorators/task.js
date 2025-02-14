213014e22106b270dd9eb476a204934e64eea3e4
Olesya Dr <odryankova@yandex.ru>
2025-02-14 03:40:43 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,24 +1,22 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-	let cache = [];
-
-	function wrapper(...args) {
-		const hash = md5(args);
-		let objectInCache = cache.find(item => item.hash === hash);
-		if (objectInCache) {
-			return "Из кеша: " + objectInCache.value;
-		}
-		let result = func(...args);
-		cache.push({
-			"hash": hash,
-			"value": result
-		});
-		if (cache.length > 5) {
-			cache.shift();
-		}
-		return "Вычисляем: " + result;
-	}
-	return wrapper;
+    const cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash == hash)
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        }
+        let result = func(...args);
+        cache.push({hash: hash, value: result});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
